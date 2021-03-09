5133b952820b515b1c1150dfb947e940545ea424
Luis Geniole <luis.geniole@gmail.com>
2021-03-09 00:04:43 -0300

    Initial commit.

---
+++ lib/sum_over_list.ex
@@ -0,0 +1,18 @@
+defmodule SumOverList do
+  @moduledoc """
+  Documentation for `SumOverList`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SumOverList.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/sum_over_list_test.exs
@@ -0,0 +1,8 @@
+defmodule SumOverListTest do
+  use ExUnit.Case
+  doctest SumOverList
+
+  test "greets the world" do
+    assert SumOverList.hello() == :world
+  end
+end
